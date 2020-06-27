4d956106d54c944fefc75a0df2f6691fa0ad013b
Edvinas Sniuolis <esniuolis.dev@gmail.com>
2020-06-27 23:16:13 +0300

    added eslint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+
 export interface HelloWorldProps {
   userName: string;
   lang: string;
 }
-export const App = (props: HelloWorldProps) => (
+
+export const App: React.FC<HelloWorldProps> = (props: HelloWorldProps) => (
   <h1>
     Hi {props.userName} from React! Welcome to {props.lang}!
   </h1>
